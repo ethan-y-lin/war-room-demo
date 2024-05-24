5ee1d5605a03aab89e4eeb20098c645c272217fa
Ethan Lin <ethan.yo.lin@gmail.com>
2024-05-24 13:04:34 -0400

    Add basic files

---
+++ src/js/script.js
@@ -0,0 +1,115 @@
+import * as THREE from 'three';
+import { ThreeMFLoader } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import * as dat from 'dat.gui';
+import stars from '../img/stars.jpg';
+import nebula from '../img/nebula.jpg';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+const roomUrl = new URL('../assets/warroom.glb', import.meta.url);
+const cleanRoomURL = new URL('../assets/roommodemodel.glb', import.meta.url);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.shadowMap.enabled = true;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45, 
+    window.innerWidth / window.innerHeight, 
+    0.1, 
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(-10, 30, 30);
+orbit.update();
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshStandardMaterial({ color: 0x00FF00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshStandardMaterial({ 
+    color: 0xFFFFFF,
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = Math.PI / 2;
+plane.receiveShadow = true;
+scene.add(plane);
+
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+directionalLight.position.set(-30, 50, 0);
+directionalLight.shadow.camera.bottom = -12;
+const dLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+scene.add(dLightHelper);
+
+const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(dLightShadowHelper);
+
+const spotLight = new THREE.SpotLight(0xFFFFFF);
+scene.add(spotLight);
+spotLight.position.set(100, 100, 0);
+spotLight.castShadow = true;
+spotLight.angle = 0.2;
+
+const sLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(sLightHelper);
+
+const assetLoader = new GLTFLoader();
+// assetLoader.load(roomUrl.href, (gltf) => {
+//     model = gltf.scene;
+//     scene.add(model);
+//     model.position.set(1, 3, 2);
+// }, undefined, (error) => {
+//     console.error(error);
+// });
+
+assetLoader.load(cleanRoomURL.href, (gltf) => {
+    model = gltf.scene;
+    scene.add(model);
+    model.position.set(5, 3, 2);
+});
+
+const gui = new dat.GUI();
+
+const options = {
+    angle: 0.2,
+    penumbra: 0,
+    intensity: 1,
+};
+
+
+gui.add(options, 'angle', 0, 1);
+gui.add(options, 'penumbra', 0, 1);
+gui.add(options, 'intensity', 0, 1);
+
+function animate(time) {
+    box.rotation.x = time/1000;
+    box.rotation.y = time/1000;
+    renderer.render(scene, camera);
+    spotLight.angle = options.angle;
+    spotLight.penumbra = options.penumbra;
+    spotLight.intensity = options.intensity;
+    sLightHelper.update();
+}
+
+
+renderer.render(scene, camera);
+renderer.setAnimationLoop(animate);
