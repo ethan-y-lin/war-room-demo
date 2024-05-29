0763d36b60bdf1eaf743ae93717ff81f6cfc6d5e
Una <yw523@cornell.edu>
2024-05-29 16:58:29 -0400

    adjust dropdown menu

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,7 +1,7 @@
-var menuHeader = document.querySelector('.menu');
-var menuContainer = document.querySelector('.menu-container');
+const menuHeader = document.querySelector('.menu-header');
+const menuContainer = document.querySelector('.menu-container');
+
 
 menuHeader.addEventListener('click', function(){
     menuContainer.classList.toggle('active');
 });
-
