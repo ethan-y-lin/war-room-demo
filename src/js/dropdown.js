a7741af436ce3849099784bea6cd7cbc562976a6
Una <yw523@cornell.edu>
2024-05-30 09:29:17 -0400

    add jquery

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,7 +1,1 @@
-const menuHeader = document.querySelector('.menu-header');
-const menuContainer = document.querySelector('.menu-container');
 
-
-menuHeader.addEventListener('click', function(){
-    menuContainer.classList.toggle('active');
-});
