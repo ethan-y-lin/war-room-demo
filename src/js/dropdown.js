a46f298f3ec4c2cbd0589a71db021f5692a8e69b
Una <yw523@cornell.edu>
2024-05-30 16:15:17 -0400

    change dropdown menu position

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,8 +1,17 @@
-$(".menu-header").on('click', function(){
-    $(this).next().toggleClass("hidden");
+$("#menu-toggle").on('click', function(){
+    $(".dropdown-menu").toggleClass("hidden");
 });
 
-// $(".menu-title").on('click', function(){
-//     $("menu-header").slideUp("slow", function(){
-//     });
-// });
+$(".menu-unit").on('click', function(){
+    if($(this).is("#fp")){
+        $("#floor-plans").toggleClass("hidden");
+    };
+    if($(this).is("#f")){
+        $("#furnitures").toggleClass("hidden");
+    };
+});
+
+$(".menu-header").on('click', function(e){
+    $(this).next().toggleClass("hidden");
+    e.stopPropagation();
+});
