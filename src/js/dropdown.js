a4c60ff1d78c02636ccf0874d534b215241ce7e5
Una <yw523@cornell.edu>
2024-05-30 12:42:15 -0400

    WIP add icon for dropdown

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,3 +1,8 @@
-$(".menu-header").on('click', function(e){
+$(".menu-header").on('click', function(){
     $(this).next().toggleClass("hidden");
-})
+});
+
+// $(".menu-title").on('click', function(){
+//     $("menu-header").slideUp("slow", function(){
+//     });
+// });
