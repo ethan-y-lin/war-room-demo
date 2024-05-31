7ed4440d42b3ce4141456d19131c21c03cfe7d7d
Una <yw523@cornell.edu>
2024-05-31 15:41:15 -0400

    add dropdown layers toggle

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,14 +1,37 @@
+//need to refresh the menu when hidden later
 $("#menu-toggle").on('click', function(){
     $(".dropdown-menu").toggleClass("hidden");
 });
 
-$(".menu-unit").on('click', function(){
+//ideally should use hover to determine hidden class but then it can't go to the next layer
+//will take a look at this later
+$(".menu-unit").on('mouseover', function(){
     if($(this).is("#fp")){
-        $("#floor-plans").toggleClass("hidden");
+        $("#floor-plans").removeClass("hidden");
+        $("#furnitures, #placeholder1, #placeholder2, #placeholder3").addClass("hidden");
     };
     if($(this).is("#f")){
         $("#furnitures").toggleClass("hidden");
+        $("#floor-plans, #placeholder1, #placeholder2, #placeholder3").addClass("hidden");
     };
+    if($(this).is("#p1")){
+        $("#placeholder1").toggleClass("hidden");
+        $("#floor-plans, #furnitures, #placeholder2, #placeholder3").addClass("hidden");
+    };
+    if($(this).is("#p2")){
+        $("#placeholder2").toggleClass("hidden");
+        $("#floor-plans, #furnitures, #placeholder1, #placeholder3").addClass("hidden");
+    };
+    if($(this).is("#p3")){
+        $("#placeholder3").toggleClass("hidden");
+        $("#floor-plans, #furnitures, #placeholder1, #placeholder2").addClass("hidden");
+    };
+});
+
+//can't hide if mouseleave on layer1 now
+//don't wanna do that cuz then it can't go to the next layer
+$(".layer2").on('mouseleave', function(){
+    $(".layer2").addClass("hidden");
 });
 
 $(".menu-header").on('click', function(e){
