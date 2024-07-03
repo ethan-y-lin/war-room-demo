f887273f3f15a22322bba47a28954f3c1e383e42
Ethan Lin <ethan.yo.lin@gmail.com>
2024-07-03 15:23:52 -0400

    Reimplement mobileControls to not use 'touchmove' event
    - it turns out that 'touchmove' does not work on
    mobile devices even though it works on the
    chrome emulator
    - I rewrote the code based on how 3js orbit
    controls gets touch based listeners.

--- src/public/javascripts/mobileControls.js
+++ src/public/javascripts/mobileControls.js
@@ -1,15 +1,25 @@
 import {
 	Euler,
 	EventDispatcher,
-	Vector3
+	Vector3, 
+    Vector2
 } from 'three';
 
 const _euler = new Euler( 0, 0, 0, 'YXZ' );
 const _vector = new Vector3();
 
 const _changeEvent = { type: 'change' };
+const _startEvent = { type: 'start' };
+const _endEvent = { type: 'end' };
 const _doubleClickEvent = {type: 'dblclick'};
 const _PI_2 = Math.PI / 2;
+const pointers = [];
+const pointerPositions = {};
+
+const rotateStart = new Vector2();
+const rotateEnd = new Vector2();
+const rotateDelta = new Vector2();
+
 
 class MobileControls extends EventDispatcher {
 
@@ -27,27 +37,23 @@ class MobileControls extends EventDispatcher {
 
 		this.pointerSpeed = 2.0;
 
-		this._onPointerMove = onPointerMove.bind( this );
-        this._onTouchMove = onTouchMove.bind( this );
-        this._onDoubleClick = onDoubleClick.bind(this);
-
 		this.connect();
 
 	}
 
 	connect() {
 
-		this.domElement.addEventListener( 'pointermove', this._onPointerMove );
-        this.domElement.addEventListener( 'touchmove', this._onTouchMove );
-        this.domElement.addEventListener('dblclick', this._onDoubleClick );
+		this.domElement.addEventListener( 'pointerdown', this.onPointerDown );
+		this.domElement.addEventListener( 'pointercancel', this.onPointerUp );
+        this.domElement.addEventListener('dblclick', this.onDoubleClick );
 
 	}
 
 	disconnect() {
 
-		this.domElement.removeEventListener( 'pointermove', this._onPointerMove );
-        this.domElement.removeEventListener( 'touchmove', this._onTouchMove );
-        this.domElement.removeEventListener('dblclick', this._onDoubleClick );
+        this.domElement.removeEventListener( 'pointerdown', this.onPointerDown );
+		this.domElement.removeEventListener( 'pointercancel', this.onPointerUp );
+        this.domElement.removeEventListener('dblclick', this.onDoubleClick );
 	}
 
 	dispose() {
@@ -93,52 +99,197 @@ class MobileControls extends EventDispatcher {
 
 	}
 
+    onPointerDown = ( event ) => {
+    
+        if ( pointers.length === 0 ) {
+    
+            this.domElement.setPointerCapture( event.pointerId );
+    
+            this.domElement.addEventListener( 'pointermove', this.onPointerMove );
+            this.domElement.addEventListener( 'pointerup', this.onPointerUp );
+    
+        }
+    
+        //
+    
+        if ( isTrackingPointer( event ) ) return;
+    
+        //
+    
+        addPointer( event );
+    
+        if ( event.pointerType === 'touch' ) {
+    
+            this.onTouchStart( event );
+    
+        } 
+    
+    }
+
+    onPointerMove = ( event ) => {
+    
+        if ( event.pointerType === 'touch' ) {
+    
+            this.onTouchMove( event );
+    
+        } 
+    
+    }
+    
+    onPointerUp = ( event ) =>{
+    
+        removePointer( event );
+    
+        switch ( pointers.length ) {
+    
+            case 0:
+    
+                this.domElement.releasePointerCapture( event.pointerId );
+    
+                this.domElement.removeEventListener( 'pointermove', this.onPointerMove );
+                this.domElement.removeEventListener( 'pointerup', this.onPointerUp );
+    
+                this.dispatchEvent( _endEvent );
+    
+                break;
+    
+            case 1:
+    
+                const pointerId = pointers[ 0 ];
+                const position = pointerPositions[ pointerId ];
+    
+                // minimal placeholder event - allows state correction on pointer-up
+                this.onTouchStart( { pointerId: pointerId, pageX: position.x, pageY: position.y } );
+    
+                break;
+    
+        }
+    
+    }
+
+    onTouchStart = (event) => {
+        trackPointer(event);
+        if (pointers.length === 1) {
+            rotateStart.set(event.pageX, event.pageY);
+        } else {
+            const position = getSecondPointerPosition( event );
+    
+            const x = 0.5 * ( event.pageX + position.x );
+            const y = 0.5 * ( event.pageY + position.y );
+    
+            rotateStart.set( x, y );
+        }
+    }
+
+    onTouchMove = (event) => {
+        trackPointer(event);
+        if ( pointers.length == 1 ) {
+    
+            rotateEnd.set( event.pageX, event.pageY );
+    
+        } else {
+    
+            const position = getSecondPointerPosition( event );
+    
+            const x = 0.5 * ( event.pageX + position.x );
+            const y = 0.5 * ( event.pageY + position.y );
+    
+            rotateEnd.set( x, y );
+    
+        }
+        rotateDelta.subVectors(rotateEnd, rotateStart)
+        this.rotateCamera(rotateDelta.x, rotateDelta.y);
+        rotateStart.copy(rotateEnd);
+    }
+
+    rotateCamera (movementX, movementY) {
+        const camera = this.camera;
+        _euler.setFromQuaternion( camera.quaternion );
+    
+        _euler.y -= movementX * 0.002 * this.pointerSpeed;
+        _euler.x -= movementY * 0.002 * this.pointerSpeed;
+    
+        _euler.x = Math.max( _PI_2 - this.maxPolarAngle, Math.min( _PI_2 - this.minPolarAngle, _euler.x ) );
+    
+        camera.quaternion.setFromEuler( _euler );
+        console.log("on pointer move")
+        this.dispatchEvent( _changeEvent );
+    }
+    
+    
+    
+    
+    onDoubleClick = (event) => {
+        this.dispatchEvent(_doubleClickEvent);
+        console.log(event);
+    }
+    
+
+    
+
 
 }
 
-// event listeners
+// Pointer Functions
 
-function onPointerMove( event ) {
+function trackPointer( event ) {
+    
+    let position = pointerPositions[ event.pointerId ];
 
-	const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-	const movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+    if ( position === undefined ) {
 
-	const camera = this.camera;
-	_euler.setFromQuaternion( camera.quaternion );
+        position = new Vector2();
+        pointerPositions[ event.pointerId ] = position;
 
-	_euler.y -= movementX * 0.002 * this.pointerSpeed;
-	_euler.x -= movementY * 0.002 * this.pointerSpeed;
+    }
 
-	_euler.x = Math.max( _PI_2 - this.maxPolarAngle, Math.min( _PI_2 - this.minPolarAngle, _euler.x ) );
-
-	camera.quaternion.setFromEuler( _euler );
-    console.log("on pointer move")
-	this.dispatchEvent( _changeEvent );
+    position.set( event.pageX, event.pageY );
 
 }
 
-function onTouchMove( event ) {
-
-	const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-	const movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-
-	const camera = this.camera;
-	_euler.setFromQuaternion( camera.quaternion );
-
-	_euler.y -= movementX * 0.002 * this.pointerSpeed;
-	_euler.x -= movementY * 0.002 * this.pointerSpeed;
-
-	_euler.x = Math.max( _PI_2 - this.maxPolarAngle, Math.min( _PI_2 - this.minPolarAngle, _euler.x ) );
-
-	camera.quaternion.setFromEuler( _euler );
-    console.log("on touch move")
-	this.dispatchEvent( _changeEvent );
+function addPointer( event ) {
+    
+    pointers.push( event.pointerId );
 
 }
 
-function onDoubleClick(event) {
-    this.dispatchEvent(_doubleClickEvent);
-    console.log(event);
+function removePointer( event ) {
+
+    delete pointerPositions[ event.pointerId ];
+
+    for ( let i = 0; i < pointers.length; i ++ ) {
+
+        if ( pointers[ i ] == event.pointerId ) {
+
+            pointers.splice( i, 1 );
+            return;
+
+        }
+
+    }
+
 }
+
+function isTrackingPointer( event ) {
+
+    for ( let i = 0; i < pointers.length; i ++ ) {
+
+        if ( pointers[ i ] == event.pointerId ) return true;
+
+    }
+
+    return false;
+
+}
+
+function getSecondPointerPosition( event ) {
+
+    const pointerId = ( event.pointerId === pointers[ 0 ] ) ? pointers[ 1 ] : pointers[ 0 ];
+
+    return pointerPositions[ pointerId ];
+
+}
+
+
 
 export { MobileControls };
